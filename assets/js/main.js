8163f2a9b16cc7dc920eb9dd9fe9877fae6ec72e
Jonatas Valerio <jonatasvalerio13@hotmail.com>
2021-06-13 16:46:33 -0300

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,8 +28,8 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
     origin: 'top',
-    distance: '80px',
-    duration: 2000,
+    distance: '100px',
+    duration: 1500,
     reset: true
 });
 
